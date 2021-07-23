47b814797bdba49be91c272336125e525c77063a
wtzeng1 <wtzeng1@gmail.com>
2021-07-23 11:51:40 +0800

    fix: 🐛 fix tracker comment

--- packages/x6/src/global/config.ts
+++ packages/x6/src/global/config.ts
@@ -8,10 +8,10 @@ export const Config = {
   /**
    * Turn on/off collect information of user client.
    *
-   * In order to serve the users better, x6 will send the URL and version
+   * In order to serve the users better, X6 will send the URL and version
    * information back to the AntV server:https://kcart.alipay.com/web/bi.do
    *
-   * Except for URL and G2 version information, no other information will
+   * Except for URL and X6 version information, no other information will
    * be collected.
    *
    * @param enabled Specifies if seed client information to AntV server.
